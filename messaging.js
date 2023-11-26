2e1b44c1ea7b89a7284b524b6d814ec095c3e70b
John Dunn <johnwdunn20@gmail.com>
2023-11-25 20:52:04 -0800

    Created test fetch request but it's backwards

--- messaging.js
+++ messaging.js
@@ -9,24 +9,24 @@
 // }
 
 //createChannel create communication channel between content script and background page of extension
-(function createChannel() {
-    //Create a port with background page for continous message communication
-    var port = chrome.extension.connect({
-        name: "Communication RQRewind" //Name for identification
-    });
+// (function createChannel() {
+//     //Create a port with background page for continous message communication
+//     var port = chrome.extension.connect({
+//         name: "Communication RQRewind" //Name for identification
+//     });
 
-    // Listen to messages from the background page
-    //When message is received, updates content of element w/ID insertmessagebutton w/content of message
-    port.onMessage.addListener(function (message) {
-      document.querySelector('#insertmessagebutton').innerHTML = message.content;
-      // port.postMessage(message);
-    });
+//     // Listen to messages from the background page
+//     //When message is received, updates content of element w/ID insertmessagebutton w/content of message
+//     port.onMessage.addListener(function (message) {
+//       document.querySelector('#insertmessagebutton').innerHTML = message.content;
+//       // port.postMessage(message);
+//     });
 
-}());
+// }());
 
-// This sends an object to the background page 
-// where it can be relayed to the inspected page
-function sendObjectToInspectedPage(message) {
-    message.tabId = chrome.devtools.inspectedWindow.tabId;
-    chrome.extension.sendMessage(message);
-}
+// // This sends an object to the background page 
+// // where it can be relayed to the inspected page
+// function sendObjectToInspectedPage(message) {
+//     message.tabId = chrome.devtools.inspectedWindow.tabId;
+//     chrome.extension.sendMessage(message);
+// }
