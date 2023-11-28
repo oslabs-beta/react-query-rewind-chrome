925e20ad974c2ed64ee4e5220fc90fddbaf9676e
John Dunn <johnwdunn20@gmail.com>
2023-11-28 14:46:58 -0800

    Added tests folder

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+};
