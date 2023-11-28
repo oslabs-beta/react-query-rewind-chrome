0998468f3d6364c0d9c2f8ba5ac235011207b1b3
eteering <teeringe@gmail.com>
2023-11-28 09:46:48 -0700

    dropdown

--- src/BasicTabs.tsx
+++ src/BasicTabs.tsx
@@ -3,7 +3,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import SimpleListMenu from './SimpleListMenu';
+import MultiSelect from './MultiSelect';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -54,7 +54,7 @@ export default function BasicTabs() {
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        <SimpleListMenu />
+        <MultiSelect />
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Metrics
