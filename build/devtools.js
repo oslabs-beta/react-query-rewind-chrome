5109fb49cd7233b8853ef2e30e0110bae758b5b0
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-11-27 16:57:06 -0800

    material ui

--- build/devtools.js
+++ build/devtools.js
@@ -4,5 +4,16 @@
 
 // Create a tab in the devtools area
 chrome.devtools.panels.create(
-    "RQRewind", null, "panel.html",
-    function(panel) {});
+  "RQRewind",
+  null,
+  "panel.html",
+  function (panel) {}
+);
+
+chrome.devtools.network.onNavigated.addListener(() => {
+    // This event is fired when the inspected window navigates to a new page.
+    // You can use it to trigger a reload of your DevTools extension
+    console.log('here')
+    // Reload your DevTools extension panel or perform necessary refresh actions here.
+    location.reload(); // This reloads the DevTools extension panel itself.
+});
