543e5431145bf4ea4277fb395561fdc0e6eefdf3
John Dunn <johnwdunn20@gmail.com>
2023-11-30 13:51:07 -0800

    Integrated json formatter into app

--- src/components/JsonFormatter.tsx
+++ src/components/JsonFormatter.tsx
@@ -1,27 +1,6 @@
 import React from 'react'
 import { JSONTree } from 'react-json-tree';
 
-// dummy data and themes
-const testData = {
-  queryKey1: {
-    post1: {
-      checked: false,
-      value: 'Test',
-      nested1: {
-        first: 1,
-        second: 2
-      }
-    },
-    post2: {
-      checked: false,
-      value: 'Test Again',
-      nested2: {
-        arr: [1,4,5] 
-      }
-    }
-  }
-}
-
 
 // https://github.com/reduxjs/redux-devtools/tree/75322b15ee7ba03fddf10ac3399881e302848874/src/react/themes
 const theme = {
@@ -50,9 +29,6 @@ type JsonFormatterType = {
 const JsonFormatter: React.FC<JsonFormatterType> = ({ jsonData }) => {
   // hideRoot: hides the root node
   // shouldExpandNodeInitially: function that determines what is initially expanded. Might be a challenge to track this from node to node
-  
-  // delete later - simply for testing
-  if (!jsonData) jsonData = testData;
 
   return (
     <div>
