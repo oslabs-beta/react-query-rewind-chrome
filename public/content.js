73cf35448d63328cef7187f368a9297ed5e707e1
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-12-07 16:58:21 -0800

    content script

--- public/content.js
+++ public/content.js
@@ -1,16 +1,31 @@
 window.addEventListener("message", (event) => {
   // Validate the message origin and structure
-  if (event.source === window && event.data.type && event.data.type === "react-query-rewind") {
+  if (
+    event.source === window &&
+    event.data.type &&
+    event.data.type === "react-query-rewind"
+  ) {
     // Handle the data
     const message = event.data.payload;
-    chrome.runtime.sendMessage(message);
+    // let port = chrome.runtime.connect({ name: 'content script'});
+    chrome.runtime.sendMessage({ sender: "content script", message: message });
     console.log("message: ", message);
   }
 });
 
-// chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-//   // This log is a test from the background script to ensure the content script is connected and working
-//   if (req.body === 'TIMETRAVEL') {
-//     window.postMessage({ body: 'TIMETRAVEL', previousState: req.previousState });
-//   }
-// });
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log("Message received:", message);
+
+  if (message.sender === "StateTab") {
+    const event = new CustomEvent("UpdateUI", {
+      detail: { queryKey: message.queryKey, queryData: message.queryData },
+    });
+    window.dispatchEvent(event);
+  }
+  if (message.sender === 'App') {
+    const event = new CustomEvent('TimeTravel', {
+      detail: {timeTravel: message.timeTravel}
+    });
+    window.dispatchEvent(event);
+  }
+});
