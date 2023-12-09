3276ce76db4a1cc27e980376eb1ad60ddc13a35d
eteering <teeringe@gmail.com>
2023-12-09 10:46:58 -0700

    tree rendering

--- public/panel.js
+++
@@ -1,9 +0,0 @@
-let port = chrome.runtime.connect({name: "devtools-panel"});
-
-port.onMessage.addListener(function(msg) {
-    const item = document.createElement('div');
-    item.innerHTML = msg.message.type;
-    document.body.appendChild(item);
-    console.log(msg.message);
-    // Handle messages from background script
-});
