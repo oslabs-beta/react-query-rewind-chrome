e22561694f9d9e2a22c5da6b2661e9eee36d444d
Rui Fan <ruifan@Ruis-MacBook-Air.local>
2023-12-09 12:29:57 -0800

    get rid of redundant code

--- public/content.js
+++ public/content.js
@@ -1,34 +1,23 @@
 //content script accesses current DOM of tab the user is currently in
 let script;
 
-window.addEventListener("message", (event) => {
-  // Validate the message origin and structure
-  if (event.source === window && event.data.type && event.data.type === "react-query-rewind") {
-    // Handle the data
-    const message = event.data.payload;
-    chrome.runtime.sendMessage(null, message);
-    // chrome.runtime.sendMessage({ sender: 'content script', message: message });
-    // console.log("message: ", message);
-  }
-});
-
 //inject script into current DOM
 const inject = (fileName) => {
   // console.log("CONTENTSCRIPT.JS: INJECTING SCRIPT");
-  script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", chrome.runtime.getURL(fileName));
+  script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', chrome.runtime.getURL(fileName));
   document.body.appendChild(script);
   // console.log("CONTENTSCRIPT.JS: SCRIPT INJECTION SUCCESSFULL");
 };
 
 //invoke inject function to inject script
-inject("inject.js");
+inject('inject.js');
 
 //wait for message from inject.js, when recieved send another message to app.tsx
-window.addEventListener("message", (event) => {
+window.addEventListener('message', (event) => {
   // console.log("message from inject.js", event.data.eventListStr);
-  if (event.data.type && event.data.type === "EVENT_LIST") {
+  if (event.data.type && event.data.type === 'EVENT_LIST') {
     // console.log("event", event);
     chrome.runtime.sendMessage({
       action: event.data.type,
@@ -41,8 +30,8 @@ window.addEventListener("message", (event) => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // Handle the message received from the popup
   // console.log("message received from popup");
-  const event = new CustomEvent("CustomEventFromContentScript", {
-    detail: { message: "Hello from content script!" },
+  const event = new CustomEvent('CustomEventFromContentScript', {
+    detail: { message: 'Hello from content script!' },
   });
   document.dispatchEvent(event);
   // console.log(message);
@@ -58,15 +47,11 @@ const windowListener = (event) => {
     // Handle the data
     const message = event.data.payload;
     chrome.runtime.sendMessage({ sender: 'content script', message: message });
-    // console.log('message: ', message);
-    chrome.runtime.sendMessage({ sender: 'content script', message: message });
-    // console.log('message: ', message);
   }
 };
 window.addEventListener('message', windowListener);
 
 const messageListener = async (message, sender, sendResponse) => {
-  // console.log('Message received:', message);
 
   if (message.sender === 'UpdateUI') {
     const event = new CustomEvent('UpdateUI', {
